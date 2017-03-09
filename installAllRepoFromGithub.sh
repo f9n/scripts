9ee310f5afd5c2fa9fd23df6d531cdc9a2e00381
pleycpl <h4rvey@protonmail.com>
2017-03-09 02:46:52 +0000

    added awk script for pure links

--- installAllRepoFromGithub.sh
+++ installAllRepoFromGithub.sh
@@ -2,20 +2,21 @@
 
 # ------------------------------------------------- #
 # We are downloading source code in link.           #
-# And created links.txt.                            #
+# And created allAtags.txt.                         #
 # ------------------------------------------------- #
-
 github="https://github.com/"
 username="pleycpl"
 query="?tab=repositories"
 githubReposLink="${github}${username}${query}"
 echo $githubReposLink
-curl $githubReposLink | grep "<a href=\"/${username}/.*" > links.txt
-cat links.txt
+curl $githubReposLink | grep "<a href=\"/${username}/.*" > allAtags.txt
+cat allAtags.txt
 
 # ------------------------------------------------- #
 # And then we strip pure link with awk.             #
 # ------------------------------------------------- #
+awk -F"\"" '{print $2}' allAtags.txt > pureLinks.txt
+cat pureLinks.txt
 
 # ------------------------------------------------- #
 # Finally we download all repos with git clone      #
