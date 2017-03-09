b8544099c3fea098b16d34a4838dbe91a6d58834
pleycpl <h4rvey@protonmail.com>
2017-03-09 22:18:42 +0000

    added hostname to links with sed

--- installAllRepoFromGithub.sh
+++ installAllRepoFromGithub.sh
@@ -4,10 +4,10 @@
 # We are downloading source code in link.           #
 # And created allAtags.txt.                         #
 # ------------------------------------------------- #
-github="https://github.com/"
+github="https://github.com"
 username="pleycpl"
 query="?tab=repositories"
-githubReposLink="${github}${username}${query}"
+githubReposLink="${github}/${username}${query}"
 echo $githubReposLink
 curl $githubReposLink | grep "<a href=\"/${username}/.*" > allAtags.txt
 cat allAtags.txt
@@ -17,6 +17,14 @@ cat allAtags.txt
 # ------------------------------------------------- #
 awk -F"\"" '{print $2}' allAtags.txt > pureLinks.txt
 cat pureLinks.txt
+
+# ------------------------------------------------- #
+# Adding host name and protocol to links!           #
+# ------------------------------------------------- #
+replace_string=".*"
+replace_with="${github}&"
+sed s~$replace_string~$replace_with~ < pureLinks.txt > pure.txt
+cat pure.txt
 
 # ------------------------------------------------- #
 # Finally we download all repos with git clone      #
