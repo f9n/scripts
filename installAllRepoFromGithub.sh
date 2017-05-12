1c2dbbd1b482530e550d2c9242dc4bb5cd58d7a0
pleycpl <h4rvey@protonmail.com>
2017-05-12 15:33:41 +0000

    updated with function style

--- installAllRepoFromGithub.sh
+++ installAllRepoFromGithub.sh
@@ -4,34 +4,54 @@
 # We are downloading source code in link.           #
 # And created allAtags.txt.                         #
 # ------------------------------------------------- #
-github="https://github.com"
-username="pleycpl"
-query="?tab=repositories"
-githubReposLink="${github}/${username}${query}"
-echo $githubReposLink
-curl $githubReposLink | grep "<a href=\"/${username}/.*" > allAtags.txt
-cat allAtags.txt
+downloadLinks() {
+    echo -n "[+] Starting downloadLinks function"
+    github="https://github.com"
+    username="pleycpl"
+    query="?tab=repositories"
+    githubReposLink="${github}/${username}${query}"
+    echo $githubReposLink
+    curl $githubReposLink | grep "<a href=\"/${username}/.*" > allAtags.txt
+    cat allAtags.txt
+}
 
 # ------------------------------------------------- #
 # And then we strip pure link with awk.             #
 # ------------------------------------------------- #
-awk -F"\"" '{print $2}' allAtags.txt > pureLinks.txt
-cat pureLinks.txt
+cleanLinks() {
+    echo -n "[+] Starting cleanLinks function"
+    awk -F"\"" '{print $2}' allAtags.txt > pureLinks.txt
+    cat pureLinks.txt
+}
 
 # ------------------------------------------------- #
 # Adding host name and protocol to links!           #
 # ------------------------------------------------- #
-replace_string=".*"
-replace_with="${github}&"
-sed s~$replace_string~$replace_with~ < pureLinks.txt > pure.txt
-cat pure.txt
+addedProtocolAndHostname() {
+    echo -n "[+] Starting addedProtocolAndHostname function"
+    replaceString=".*"
+    replaceWith="${github}&"
+    sed s~$replaceString~$replaceWith~ < pureLinks.txt > pure.txt
+    cat pure.txt
+}
 
 # ------------------------------------------------- #
 # Finally we download all repos with git clone      #
 # --------------------------------------------------#
-for f in `cat pure.txt`
-do
-    git clone $f
-done
+downloadRepos() {
+    echo -n "[+] Downloading repos..."
+    for f in `cat pure.txt`
+    do
+        git clone $f
+    done
+}
 
+Main() {
+    downloadLinks
+    cleanLinks
+    addedProtocolAndHostname
+    downloadRepos
+}
+
+Main
 
