5e760d4b5be618614461e4b3f5d99a62502685a9
pleycpl <h4rvey@protonmail.com>
2017-05-13 12:18:54 +0000

    reading directory in console

--- updateReposInDirectory.sh
+++ updateReposInDirectory.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # http://stackoverflow.com/questions/14352290/listing-only-directories-using-ls-in-bash-an-examination
-for i in $(ls -d */.git)
-do 
+
+read -p "Entry Directory: " directory
+directory="${directory}/*/.git"
+
+for i in $(ls -d ${directory})
+do
     echo ${i%%/.git}
     cd ${i%%/.git}
     git pull
-    cd ..
+    cd -
 done
