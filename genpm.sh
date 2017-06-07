fc5fc26adb25020911869fae919b350a80934994
pleycpl <h4rvey@protonmail.com>
2017-06-07 15:14:54 +0000

    added init genpm script

---
+++ genpm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# We are using these tutorials:
+# https://www.digitalocean.com/community/tutorials/package-management-basics-apt-yum-dnf-pkg
+# https://www.linode.com/docs/tools-reference/linux-package-management/
+# https://en.0wikipedia.org/index.php?q=aHR0cHM6Ly9lbi53aWtpcGVkaWEub3JnL3dpa2kvTGlzdF9vZl9zb2Z0d2FyZV9wYWNrYWdlX21hbmFnZW1lbnRfc3lzdGVtcw
+
+# General package manager (genpm)
+# For now, we supported unix-like os.
+# Inclusive of Tools:
+# Arch based    : pacman
+# Debian based  : apt-get
+# Rpm based     : yum
+# Fedora based  : dnf
+# FreeBsd based : pkg
+
+# ------------------------------------------- #
+##########    Genpm Command Lists    ##########
+# $ genpm install <pkg_name>                  #
+# $ genpm remove <pkg_name>                   #
+# $ genpm search <search_string>              #
+# ------------------------------------------- #
