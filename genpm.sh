76420ed53e8e2254b3578d47b1d00ebf2570c4c2
pleycpl <h4rvey@protonmail.com>
2017-06-07 15:22:17 +0000

    updated comments

--- genpm.sh
+++ genpm.sh
@@ -4,14 +4,20 @@
 # https://www.linode.com/docs/tools-reference/linux-package-management/
 # https://en.0wikipedia.org/index.php?q=aHR0cHM6Ly9lbi53aWtpcGVkaWEub3JnL3dpa2kvTGlzdF9vZl9zb2Z0d2FyZV9wYWNrYWdlX21hbmFnZW1lbnRfc3lzdGVtcw
 
-# General package manager (genpm)
-# For now, we supported unix-like os.
-# Inclusive of Tools:
-# Arch based    : pacman
-# Debian based  : apt-get
-# Rpm based     : yum
-# Fedora based  : dnf
-# FreeBsd based : pkg
+# ------------------------------------------- #
+# General package manager (genpm)             #
+# For now, we supported unix-like os.         #
+# Inclusive of Tools                          #
+### Linux                                     #
+# Arch based    : pacman                      #
+# Debian based  : apt-get                     #
+# Rpm based     : yum                         #
+# Fedora based  : dnf                         #
+### Bsd                                       #
+# FreeBsd based : pkg                         #
+### Apple                                     #
+# macOs : homebrew                            #
+# ------------------------------------------- #
 
 # ------------------------------------------- #
 ##########    Genpm Command Lists    ##########
