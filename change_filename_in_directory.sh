91cb766861fc23fda09df70803f9eb4cffb9e81e
h4rvey r0ss <h4rvey@protonmail.com>
2017-09-20 15:21:30 +0000

    Update change_filename_in_directory.sh

--- change_filename_in_directory.sh
+++ change_filename_in_directory.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 for file in $(find . -name "updatefilelist.txt"); do
-    filename=${file##*/} # Learn filename in string path
-    path=${file%$filename}  # Learn up directory
+    filename=${file##*/} # Learn filename in string path,   filename=$(basename $file)
+    path=${file%$filename}  # Learn up directory            updirname=$(dirname $file)
     variable="filelist.txt"
     mv $file ${path}${variable}
 done
