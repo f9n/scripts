65ec365c26e79b796389811f8766a6f0a7162ec8
pleycpl <h4rvey@protonmail.com>
2017-10-08 15:26:01 +0000

    created init, main,... functions

--- offlinePip3forPython3.sh
+++ offlinePip3forPython3.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
 
-All_Py2_SitePackages=$(find $HOME -name site-packages -type d | grep 'python2' )
-All_Py3_SitePackages=$(find $HOME -name site-packages -type d | grep 'python3' )
+All_Py2_SitePackages_PATHS=""
+All_Py3_SitePackages_PATHS=""
 
-echo "Python2"
-for SitePackages in ${All_Py2_SitePackages}; do
-    echo ${SitePackages}
-done
+function init() {
+    echo "[+] init function"
+    All_Py2_SitePackages_PATHS=$(find $HOME -name site-packages -type d | grep 'python2' )
+    All_Py3_SitePackages_PATHS=$(find $HOME -name site-packages -type d | grep 'python3' )
+}
+function get_py2_site_packages_paths {
+    echo "Python2"
+    for SitePackages in ${All_Py2_SitePackages_PATHS}; do
+        echo ${SitePackages}
+    done
+}
+function get_py3_site_packages_paths {
+    echo "Python3"
+    for SitePackages in ${All_Py3_SitePackages_PATHS}; do
+        echo ${SitePackages}
+    done
+}
 
-echo "Python3"
-for SitePackages in ${All_Py3_SitePackages}; do
-    echo ${SitePackages}
-done
+function main {
+    init
+    get_py2_site_packages_paths
+    get_py3_site_packages_paths
+}
 
+main "$@"
