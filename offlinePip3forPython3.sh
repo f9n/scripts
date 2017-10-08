b4c783d9d5a987dc78c69fe0248d5bd74b951849
pleycpl <h4rvey@protonmail.com>
2017-10-08 18:53:30 +0000

    added read_all_package_path(), updated other functions

--- offlinePip3forPython3.sh
+++ offlinePip3forPython3.sh
@@ -24,6 +24,7 @@ function usage {
 
 function read_init_files {
     local path=/tmp/venv/lib/python3*/site-packages
+    echo "[+] read_init_files"
     #virtualenv /tmp/venv
     for initial_file in $(ls $path); do
         InitialFiles+=($initial_file)
@@ -31,10 +32,19 @@ function read_init_files {
 }
 
 function read_downloaded_packages_in_site_packages {
-    local path=~/Documents/Tadimlik/flask_mysql_example/venv/lib/python3.6/site-packages
+    local path=$1
+    local outputfile=$2
+    echo "[+] read_downloaded_packages_in_site_packages"
     for file in $(ls $path); do
         [[ "${InitialFiles[@]}" == *"$file"* ]] && continue
-        echo ${path}/${file} >> packagelist.txt
+        echo $file ${path}/${file} >> $outputfile
+    done
+}
+function read_all_package_path {
+    local python_version=$1
+    echo "[+] read_all_package_path, python_version: $python_version"
+    for Site_Packages_Path in ${ALL_SITE_PACKAGES_PATHS[$python_version]}; do
+        read_downloaded_packages_in_site_packages $Site_Packages_Path packagelist_${python_version}.txt
     done
 }
 
@@ -43,12 +53,12 @@ function main {
     local my_venv_path=$2 # Currently, Please insert full path. Like /home/<user_name>/....
     local status=$3
     local package_name=$4 # We must learn sub packages this package_name.
-    #init
+    init
     #print_site_packages_paths $python_status
     read_init_files
     echo ${InitialFiles[@]}
-    rm packagelist.txt
-    read_downloaded_packages_in_site_packages
+    rm packagelist_py2.txt packagelist_py3.txt 2> /dev/null
+    read_all_package_path $python_status
 }
 
 main "$@"
