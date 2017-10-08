6bf9a8c659cb8e21ff6549d627cb46aadc3b4ada
pleycpl <h4rvey@protonmail.com>
2017-10-08 18:17:06 +0000

    read_init_files() ve read_downloaded_packages...() fonksiyonu eklendi. InitialFiles global degiskeni olusturuldu.

--- offlinePip3forPython3.sh
+++ offlinePip3forPython3.sh
@@ -4,6 +4,9 @@ declare -A ALL_SITE_PACKAGES_PATHS=(
     ["py2"]=""
     ["py3"]=""
 )
+
+declare -a InitialFiles=("")
+
 function init() {
     echo "[+] init function"
     ALL_SITE_PACKAGES_PATHS["py2"]=$(find $HOME -name site-packages -type d | grep 'python2' )
@@ -18,14 +21,43 @@ function print_site_packages_paths {
 function usage {
     echo "./main.sh <py3|py2> <my_venv_path> <install|remove> <package_name>"
 }
+
+function read_init_files {
+    local path=/tmp/venv/lib/python3*/site-packages
+    #virtualenv /tmp/venv
+    for initial_file in $(ls $path); do
+        InitialFiles+=($initial_file)
+    done
+}
+
+function read_downloaded_packages_in_site_packages {
+    local path=~/Documents/Tadimlik/flask_mysql_example/venv/lib/python3.6/site-packages
+    for file in $(ls $path); do
+        [[ "${InitialFiles[@]}" == *"$file"* ]] && continue
+        echo ${path}/${file} >> packagelist.txt
+    done
+}
+
 function main {
     local python_status=$1 # Like py3 or py2
     local my_venv_path=$2 # Currently, Please insert full path. Like /home/<user_name>/....
     local status=$3
     local package_name=$4 # We must learn sub packages this package_name.
-    init
-    print_site_packages_paths $python_status
+    #init
+    #print_site_packages_paths $python_status
+    read_init_files
+    echo ${InitialFiles[@]}
+    rm packagelist.txt
+    read_downloaded_packages_in_site_packages
 }
 
 main "$@"
 
+# ...../site-packages/<package_name>
+# ...../site-packages/<package_name>-<version>.dist-info
+# flask             Flask-0.12.2.dist-info
+# itsdangerous.py   itsdangerous-0.24.dist-info
+# flask_mysqldb     Flask_MySQLdb-0.2.0.dist-info
+# markupsafe        MarkupSafe-1.0.dist-info
+# jinja2            Jinja2-2.9.6.dist-info
+# click             click-6.7.dist-info
