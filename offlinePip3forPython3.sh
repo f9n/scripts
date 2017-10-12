b2fd5be206c7950218bbb4a1daa812eee17526e6
pleycpl <h4rvey@protonmail.com>
2017-10-12 20:33:56 +0000

    added some comment

--- offlinePip3forPython3.sh
+++ offlinePip3forPython3.sh
@@ -4,28 +4,32 @@ declare -A ALL_SITE_PACKAGES_PATHS=(
     ["py2"]=""
     ["py3"]=""
 )
-
 declare -a InitialFiles=("")
 
 function init() {
+    # Set up global arrays, finding all py3 or py2 site-packages path 
     echo "[+] init function"
     ALL_SITE_PACKAGES_PATHS["py2"]=$(find $HOME -name site-packages -type d | grep 'python2' )
     ALL_SITE_PACKAGES_PATHS["py3"]=$(find $HOME -name site-packages -type d | grep 'python3' )
 }
 function print_site_packages_paths {
+    # printing "site_packages" paths in global ALL_SITE_PACKAGES_PATHS for python version
     local python_version=$1
+    echo "[+] print_site_packages_paths() python_version: $python_version"
     for Site_Packages_Path in ${ALL_SITE_PACKAGES_PATHS[$python_version]}; do
         echo ${Site_Packages_Path}
     done
 }
 function usage {
+    # bash offlinePip3forPython3.sh py3 ~/venv install telepot
     echo "./main.sh <py3|py2> <my_venv_path> <install|remove> <package_name>"
 }
 
 function read_init_files {
+    # reading virtualenv's init files
     local path=/tmp/venv/lib/python3*/site-packages
-    echo "[+] read_init_files"
-    #virtualenv /tmp/venv
+    echo "[+] read_init_files path: $path"
+    virtualenv /tmp/venv
     for initial_file in $(ls $path); do
         InitialFiles+=($initial_file)
     done
@@ -34,9 +38,9 @@ function read_init_files {
 function read_downloaded_packages_in_site_packages {
     local path=$1
     local outputfile=$2
-    echo "[+] read_downloaded_packages_in_site_packages"
+    echo "[+] read_downloaded_packages_in_site_packages() path: $path outputfile: $outputfile"
     for file in $(ls $path); do
-        [[ "${InitialFiles[@]}" == *"$file"* ]] && continue
+        [[ "${InitialFiles[@]}" == *"$file"* ]] && continue # if it is init file, it doesn't adding "packagelist_py2.txt" or "packagelist_py3.txt" file
         echo $file ${path}/${file} >> $outputfile
     done
 }
@@ -44,12 +48,15 @@ function read_all_package_path {
     local python_version=$1
     echo "[+] read_all_package_path, python_version: $python_version"
     for Site_Packages_Path in ${ALL_SITE_PACKAGES_PATHS[$python_version]}; do
+        # path of all package in "site_packages" is adding to "packagelist_py2.txt" or "packagelist_py3.txt" file.
         read_downloaded_packages_in_site_packages $Site_Packages_Path packagelist_${python_version}.txt
     done
 }
 function install {
+    # installing package
     local package_name=$1
     local python_version=$2
+    echo "[+] install() package_name: $package_name python_version: $python_version"
     for line in $( awk '{ print NR, $1}' packagelist_${python_version}.txt |\
                     grep -i $package_name |\
                     awk '{ print $1 }'); do
@@ -58,12 +65,12 @@ function install {
     done
 }
 function main {
-    local python_status=$1 # Like py3 or py2
-    local my_venv_path=$2 # Currently, Please insert full path. Like /home/<user_name>/....
-    local status=$3
-    local package_name=$4 # We must learn sub packages this package_name.
+    local python_status=$1  # Like py3 or py2
+    local my_venv_path=$2   # Currently, Please insert full path. Like /home/<user_name>/....
+    local status=$3         # "install" or "remove" 
+    local package_name=$4   # package_name.
     init
-    #print_site_packages_paths $python_status
+    print_site_packages_paths $python_status
     read_init_files
     echo ${InitialFiles[@]}
     rm packagelist_py2.txt packagelist_py3.txt 2> /dev/null
